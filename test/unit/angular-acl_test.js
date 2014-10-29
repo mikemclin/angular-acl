1e013edbe4399eeaf16b6ba779f0f13792a7d0bd
Mike McLin <mclinmike@gmail.com>
2014-10-28 22:04:37 -0400

    Add some more unit tests

--- test/unit/angular-acl_test.js
+++ test/unit/angular-acl_test.js
@@ -9,12 +9,37 @@ describe('AclService', function () {
     AclService = _AclService_;
   }));
 
+  describe('roleHasAbilities()', function () {
+
+    it('should return true data object has role as a property with an array value', function () {
+      expect(AclService._roleHasAbilities('foo')).toBeFalsy();
+      AclService._data.abilities.foo = [];
+      expect(AclService._roleHasAbilities('foo')).toBeTruthy();
+    });
+
+  });
+
+  describe('getRoleAbilities()', function () {
+
+    it('should return the role abilities array', function () {
+      AclService._data.abilities.foo = ['bar', 'baz'];
+      var abilities = AclService._getRoleAbilities('foo');
+      expect(abilities).toEqual(['bar', 'baz']);
+    });
+
+    it('should return empty array when role does not exist', function () {
+      var abilities = AclService._getRoleAbilities('foo');
+      expect(abilities).toEqual([]);
+    });
+
+  });
+
   describe('attachRole()', function () {
 
     it('should add role to current session', function () {
-      expect(AclService.hasRole('foo')).toBeFalsy();
+      expect(AclService._data.roles.indexOf('foo')).toEqual(-1);
       AclService.attachRole('foo');
-      expect(AclService.hasRole('foo')).toBeTruthy();
+      expect(AclService._data.roles.indexOf('foo')).toEqual(0);
     });
 
   });
